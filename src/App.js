f17a2c31c37348f09bdfa20b8dc8e97188a3dab3
Nouf AlSalem <61601877+noufalsalem@users.noreply.github.com>
2022-08-15 13:19:56 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,7 @@ import './static/style/main.css';
 class App extends React.Component {
   render() {
     return (
-      <>
-        <div id="article">
-          <h1>Good Luck!</h1>
-          <div>
-            <p>“Java is to JavaScript what Car is to Carpet.”</p>
-            <p>— Chris Heilmann</p>
-          </div>
-        </div>
-      </>
+  
     )
   }
 }
